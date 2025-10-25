2490771015e4dc44b9d860284521ed40ad9f902c
Simone Ingenito <simoneingenito@10-20-35-122.0100.fed.it.iosda.org>
2025-10-25 16:00:45 +0200

    Primo push

---
+++ backend/server/types/hr.type.ts
@@ -0,0 +1,12 @@
+export type HR = {
+    email: string,
+    firstname: string,
+    lastname: string, 
+    password: string,
+
+    companyName: string, 
+    operationalField: string
+
+    companyDescription?: string, 
+    
+}
